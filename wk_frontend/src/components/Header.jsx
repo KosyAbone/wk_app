a8ad19c3dd3bfdc191aa7efda2e70bdff7de3e32
Kosiso Abone <abonekosiso@gmail.com>
2024-02-14 15:31:56 -0500

    changed a tags to Link to

--- wk_frontend/src/components/Header.jsx
+++ wk_frontend/src/components/Header.jsx
@@ -1,17 +1,25 @@
 import {Navbar, Nav, Container} from 'react-bootstrap'
 import {FaShoppingCart, FaUser} from 'react-icons/fa'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>  
             <Container>
-                <Navbar.Brand href="/">WorkingklassNG</Navbar.Brand>
+                <LinkContainer to='/'>
+                    <Navbar.Brand href="/">WorkingklassNG</Navbar.Brand>
+                </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
-                        <Nav.Link href="/cart"><FaShoppingCart />Cart</Nav.Link>
+                        <LinkContainer to='/cart'>
+                            <Nav.Link href="/cart"><FaShoppingCart /> Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
                         <Nav.Link href="/login"><FaUser /> Sign In</Nav.Link>
+                        </LinkContainer> 
                     </Nav>
                 </Navbar.Collapse>
             </Container>    
