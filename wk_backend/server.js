9b1bfc2ce81655851d389c02a1a699c55f7de6cb
Kosiso Abone <abonekosiso@gmail.com>
2024-02-15 22:32:16 -0500

    Started backend structuring

--- wk_backend/server.js
+++ wk_backend/server.js
@@ -1,1 +1,8 @@
-console.log('Server is running');
+import express from 'express';
+const port = 6000;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('This is the home page'))
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
