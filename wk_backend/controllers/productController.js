8dbd786844fdc46facf1be5ce8b4031559b031c9
Kosiso Abone <abonekosiso@gmail.com>
2024-02-23 16:35:22 -0500

    created custom asyn handler middleware and changed data fetch from dummy data to actual MongoDB data

--- wk_backend/controllers/productController.js
+++ wk_backend/controllers/productController.js
@@ -1,12 +1,19 @@
-import products from "../products.js";
+import Product from "../models/productModel.js";
+import asyncHandler from "../middleware/asyncHandler.js";
 
-const getProducts = async (req, res) => {
+
+const getProducts = asyncHandler(async (req, res) => {  
+  const products = await Product.find({});
   res.json(products);
-}
+})
 
-const getProductById = async (req, res) => {
-  const product = products.find(product => product._id === req.params.id);
-  res.json(product);
-}
+const getProductById = asyncHandler(async (req, res) => { 
+  const product = await Product.findById(req.params.id);
+  if(product) {
+    return res.json(product);
+  } else {
+    res.status(404).json({message: "Product not found"});
+  }
+})
 
 export { getProducts, getProductById };
