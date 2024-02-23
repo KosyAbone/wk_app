8232f41ead6354f27135a06966293e9629b7ff45
Kosiso Abone <abonekosiso@gmail.com>
2024-02-23 15:47:36 -0500

    created function to destry data

--- wk_backend/seeder.js
+++ wk_backend/seeder.js
@@ -28,3 +28,17 @@ const importData = async () => {
         process.exit(1);
     }
 }
+
+const destroyData = async () => {
+    try{
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+        console.log("Data successfully Destroyed!".red.inverse);
+    }
+    catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
+}
+
