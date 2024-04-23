dcf5c8852464b62bb23f538694d924ed5dcf5ed1
Kosiso Abone <abonekosiso@gmail.com>
2024-04-22 23:59:31 -0400

    apiSlice unit

--- wk_frontend/src/slices/apiSplice.js
+++ wk_frontend/src/slices/apiSplice.js
@@ -3,8 +3,8 @@ import { BASE_URL } from '../constants';
 
 const baseQuery = fetchBaseQuery({baseUrl: BASE_URL});
 
-export const apiSlice = createApi({
-    baseQuery,
-    tagTypes: ['Product', 'User', 'Order'],
-    endpoints: (builder) => ({})
-})
+// export const apiSlice = createApi({
+//     baseQuery,
+//     tagTypes: ['Product', 'User', 'Order'],
+//     endpoints: (builder) => ({})
+// })
